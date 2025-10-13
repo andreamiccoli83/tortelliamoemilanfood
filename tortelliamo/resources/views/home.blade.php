5b8cc018c359f4accd3a9be8b8c5b89995394580
Andrea Miccoli <miccoli.andrea83@gmail.com>
2025-10-13 19:45:26 +0200

    pagina come html

--- tortelliamo/resources/views/home.blade.php
+++ tortelliamo/resources/views/home.blade.php
@@ -362,6 +362,295 @@
                 </div>
             </div>
         </div>
+        <div class="box-post style-3" data-wow-delay="0.3s">
+            <div class="wrap-content">
+                <div class="content wow fadeInUp">
+                    <h2 class="title">LA NOSTRA STORIA</h2>
+                    <p>
+                        La nostra gastronomia nasce dalla passione per le ricette tradizionali emiliane, 
+                        tramandate di generazione in generazione e preparate con ingredienti selezionati del nostro territorio.
+                    </p>
+                </div>
+                <div class="wrap-image">
+                    <img class="lazyload" 
+                         data-src="{{ asset('images/nuove/storia1.png') }}"
+                         src="{{ asset('images/nuove/storia1.png') }}" 
+                         alt="La nostra storia" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Best Selling Products -->
+    <section class="section-best-selling tf-spacing-2 pt-0">
+        <div class="tf-container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="heading-section">
+                        <p class="sub-title wow fadeInUp">I NOSTRI PRODOTTI PIÙ VENDUTI</p>
+                        <div class="title text-4 wow fadeInUp">CONSIGLIATI DALLO CHEF</div>
+                        <p class="wow fadeInUp">
+                            Scoprite le nostre specialità più amate, preparate secondo le ricette tradizionali emiliane.<br>
+                            I nostri chef hanno creato un universo di sapori autentici che vi conquisteranno!
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/tortelli_zucca.png') }}"
+                                 src="{{ asset('images/nuove/tortelli_zucca.png') }}" 
+                                 alt="Tortelli di Zucca" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€4.00</div>
+                            <h4 class="name"><a href="#">TORTELLI DI ZUCCA</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/cappelletti_brodo.png') }}"
+                                 src="{{ asset('images/nuove/cappelletti_brodo.png') }}" 
+                                 alt="Cappelletti in Brodo" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€6.12</div>
+                            <h4 class="name"><a href="#">CAPPELLETTI IN BRODO</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/lasagne.png') }}"
+                                 src="{{ asset('images/nuove/lasagne.png') }}" 
+                                 alt="Lasagne" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€5.50</div>
+                            <h4 class="name"><a href="#">LASAGNE</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/tagliatelle_ragu.png') }}"
+                                 src="{{ asset('images/nuove/tagliatelle_ragu.png') }}" 
+                                 alt="Tagliatelle al Ragù" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€3.54</div>
+                            <h4 class="name"><a href="#">TAGLIATELLE AL RAGÙ</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/tortellini_ragu.png') }}"
+                                 src="{{ asset('images/nuove/tortellini_ragu.png') }}" 
+                                 alt="Tortellini al Ragù" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€8.50</div>
+                            <h4 class="name"><a href="#">TORTELLINI AL RAGÙ</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="product-item wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="wrap-image">
+                            <img class="lazyload" 
+                                 data-src="{{ asset('images/nuove/parmigiana_melanzane.png') }}"
+                                 src="{{ asset('images/nuove/parmigiana_melanzane.png') }}" 
+                                 alt="Parmigiana di Melanzane" />
+                        </div>
+                        <div class="content">
+                            <div class="price">€6.90</div>
+                            <h4 class="name"><a href="#">PARMIGIANA DI MELANZANE</a></h4>
+                            <div class="rating">
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                                <i class="icon-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item1 wow fadeInRight" data-wow-duration="3s">
+            <img src="{{ asset('images/items/item-section-best-selling.png') }}" alt="">
+        </div>
+        <div class="item2 wow fadeInLeft" data-wow-duration="3s">
+            <img src="{{ asset('images/items/item-section-best-selling2.png') }}" alt="">
+        </div>
+    </section>
+
+    <!-- Section Testimonials -->
+    <section class="section-testimonial tf-spacing-6">
+        <div class="swiper-container slide-testimonials">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="icon wow fadeInUp">
+                            <img src="{{ asset('icons/blockquote.svg') }}" alt="">
+                        </div>
+                        <p class="paragraph wow fadeInUp">
+                            Ordiniamo sempre la pasta fresca da Tortelliamo per le cene in famiglia e non siamo mai delusi! 
+                            I tortelli di zucca e i cappelletti in brodo sono semplicemente ECCEZIONALI! Ottimo lavoro. 
+                            Sicuramente torneremo presto per provare le altre specialità che preparate con tanta passione e tradizione!
+                        </p>
+                        <h5 class="wow fadeInUp"><a href="#">MARCO BIANCHI</a></h5>
+                        <div class="rating wow fadeInUp">
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <div class="icon wow fadeInUp">
+                            <img src="{{ asset('icons/blockquote.svg') }}" alt="">
+                        </div>
+                        <p class="paragraph wow fadeInUp">
+                            La qualità dei prodotti è eccellente! La pasta fresca è sempre perfetta e le lasagne sono divine. 
+                            Il servizio di consegna è puntuale e il personale molto gentile. Consigliatissimo!
+                        </p>
+                        <h5 class="wow fadeInUp"><a href="#">GIULIA ROSSI</a></h5>
+                        <div class="rating wow fadeInUp">
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                            <i class="icon-star"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-button-next button section-testimonial-next">
+                <i class="icon-arrow-draw"></i>
+            </div>
+            <div class="swiper-button-prev button section-testimonial-prev">
+                <i class="icon-arrow-draw"></i>
+            </div>
+        </div>
+        <div class="wrap-service-item">
+            <div class="swiper-container slide-layout-3 mb-0">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="service-item wow fadeInUp">
+                            <div class="wrap-image">
+                                <img class="lazyload" 
+                                     data-src="{{ asset('images/nuove/tradizione.png') }}"
+                                     src="{{ asset('images/nuove/tradizione.png') }}" 
+                                     alt="Tradizione" />
+                            </div>
+                            <div class="content">
+                                <div class="icon">
+                                    <i class="icon-croissant"></i>
+                                </div>
+                                <h2>IL GUSTO DELLA TRADIZIONE</h2>
+                                <p>
+                                    Da generazioni, la nostra passione per la pasta fresca continua immutata, 
+                                    utilizzando solo ingredienti genuini e ricette tradizionali.
+                                </p>
+                                <a class="tf-btn-default" href="#">SCOPRI DI PIÙ<i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="service-item wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="wrap-image">
+                                <img class="lazyload" 
+                                     data-src="{{ asset('images/nuove/consegna.png') }}"
+                                     src="{{ asset('images/nuove/consegna.png') }}" 
+                                     alt="Consegna" />
+                            </div>
+                            <div class="content">
+                                <div class="icon">
+                                    <i class="icon-shopping-bag"></i>
+                                </div>
+                                <h2>CONSEGNA GRATUITA</h2>
+                                <p>
+                                    Ordina online o telefonicamente e ricevi i nostri prodotti freschi direttamente a casa tua, 
+                                    con consegna gratuita nel comune di Novellara.
+                                </p>
+                                <a class="tf-btn-default" href="#">SCOPRI DI PIÙ<i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="service-item wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="wrap-image">
+                                <img class="lazyload" 
+                                     data-src="{{ asset('images/section/service3.jpg') }}"
+                                     src="{{ asset('images/section/service3.jpg') }}" 
+                                     alt="Novità" />
+                            </div>
+                            <div class="content">
+                                <div class="icon">
+                                    <i class="icon-blog"></i>
+                                </div>
+                                <h2>ULTIME NOVITÀ</h2>
+                                <p>
+                                    Seguici sui nostri canali social per scoprire le novità e le specialità stagionali 
+                                    che prepariamo ogni giorno.
+                                </p>
+                                <a class="tf-btn-default" href="#">SCOPRI DI PIÙ<i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
     <!-- Section Location/Contact -->
@@ -408,7 +697,15 @@
                 </div>
                 <div class="col-xl-4">
                     <div class="map-box relative h-full wow fadeInRight">
-                        <div id="map">MAPPA</div>
+                        <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2826.8!2d10.7333!3d44.8500!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478014e0a0a0a0a0%3A0x0!2sPiazza%20Cesare%20Battisti%203%2C%20Novellara%2C%20RE!5e0!3m2!1sit!2sit!4v1234567890" 
+                            width="100%" 
+                            height="100%" 
+                            style="border:0; min-height: 400px;" 
+                            allowfullscreen="" 
+                            loading="lazy" 
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
                     </div>
                 </div>
             </div>
